75753e31a78b40d8fb8be0cf5f8eed93260330d1
Matías <matiaspendino76@gmail.com>
2023-02-26 12:41:21 -0300

    Detalle de receta creado. Creación modelo PasoReceta.

--- CocinaSalud/apps/receta/models.py
+++ CocinaSalud/apps/receta/models.py
@@ -7,7 +7,6 @@ class Receta(BaseModel):
     imagen_principal = models.ImageField(upload_to='recetas/')
     resumen = models.TextField(blank=True)
     ingredientes = models.TextField(blank=True)
-    instrucciones = models.TextField(blank=True)
     composicion_nutricional = models.TextField(blank=True)
     duracion = models.IntegerField(blank=True)
     imagen_secundaria_1 = models.ImageField(blank=True, upload_to='recetas/')
@@ -21,3 +20,17 @@ class Receta(BaseModel):
 
     def __str__(self):
         return f"Receta de {self.titulo}"
+    
+
+class PasoReceta(BaseModel):
+    titulo = models.CharField(max_length=255)
+    numero_paso = models.PositiveSmallIntegerField()
+    receta = models.ForeignKey(Receta, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return f'{self.receta}: Paso {self.numero_paso}'
+
+    class Meta:
+        verbose_name = 'Paso receta'
+        verbose_name_plural = 'Pasos receta'
--- CocinaSalud/apps/receta/views.py
+++ CocinaSalud/apps/receta/views.py
@@ -12,3 +12,18 @@ class RecetasIndex(ListView):
 
 def ver_receta(request, receta_id):
     receta = Receta.objects.filter(id=receta_id).first()
+    pasos_receta = PasoReceta.objects.filter(receta__id=receta_id).order_by('numero_paso')
+
+    # La imagen secundaria horizontal aparecerá entre medio de las instrucciones
+    if pasos_receta.count() % 2 == 0:
+        posicion_imagen = pasos_receta.count() / 2
+    else:
+        posicion_imagen = (pasos_receta.count() + 1) / 2
+
+    context = {
+        'receta': receta,
+        'pasos': pasos_receta,
+        'posicion_imagen': posicion_imagen
+    }
+
+    return render(request, 'receta_detalle.html', context)
