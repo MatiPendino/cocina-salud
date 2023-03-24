417d06aa6b1100b6dca8104e13dd702434e134a9
Matías <matiaspendino76@gmail.com>
2023-03-24 15:27:24 -0300

    Reestructure HTML/CSS.

--- CocinaSalud/apps/sitio/urls.py
+++ CocinaSalud/apps/sitio/urls.py
@@ -3,8 +3,8 @@ from apps.sitio import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("acerca_de/", views.acerca_de, name="acerca_de"),
-    path("signup", views.signup, name="register"),
+    path("acerca_de", views.acerca_de, name="acerca_de"),
+    path("signup", views.signup, name="signup"),
     path("signin", views.signin, name="signin"),
-    path("sign_out", views.sign_out, name="sign_out")
+    path("sign_out", views.sign_out, name="signout")
 ]
