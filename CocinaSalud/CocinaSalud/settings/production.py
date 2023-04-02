5e904cadb145ed1fc38c58e04dd8511e3e96962a
Matías <matiaspendino76@gmail.com>
2023-04-02 22:40:53 +0000

    Deployment corrections.

--- CocinaSalud/CocinaSalud/settings/production.py
+++ CocinaSalud/CocinaSalud/settings/production.py
@@ -2,37 +2,25 @@ from .base import *
 from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
     'localhost',
-    '161.35.123.98',
     '192.168.0.165',
-    '161.35.51.95'
+    '161.35.51.95',
+    'cocinasalud.net',
+    'www.cocinasalud.net',
+    '*.cocinasalud.net',
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CocinaSalud',
-        'USER': 'mati',
+        'USER': 'root',
+        'PASSWORD': config('DATABASE_PASSWORD')
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': "localhost",
-        "PORT": "3306",
-        "USER": "root",
-        "PASSWORD": "",
-        "NAME": "cocina_salud",
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
