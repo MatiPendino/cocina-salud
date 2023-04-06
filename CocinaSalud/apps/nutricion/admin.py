fd29b342ee50a1c3bd3b2cdb5edfd663dcf5a36c
Matías <matiaspendino76@gmail.com>
2023-04-06 10:58:26 -0300

    Import/Export objects in Django Admin.

--- CocinaSalud/apps/nutricion/admin.py
+++ CocinaSalud/apps/nutricion/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
-admin.site.register(Nutricion)
+class NutricionResources(resources.ModelResource):
+    class Meta:
+        model = Nutricion
+
+
+class NutricionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ('titulo',)
+    list_display = ('titulo', 'creation_date', 'state')
+    resource_class = NutricionResources
+
+admin.site.register(Nutricion, NutricionAdmin)
