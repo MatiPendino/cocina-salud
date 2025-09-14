4c8a53750a94280d75c52c74930a65a529d6ce1a
Mati <matiaspendino76@gmail.com>
2025-09-14 10:18:48 +0200

    config: postgresql integration

--- CocinaSalud/settings/production.py
+++ CocinaSalud/settings/production.py
@@ -9,8 +9,16 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_NAME'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+            'options': '-c search_path=cocina'
+        }
     }
 }
 
